6b74f60cf722fd19130f74fd4a8caa0b7648857f
Pavananon <killkinggamer@gmail.com>
2023-02-18 20:27:57 +0700

    map color

--- data_analize/test_poom_map.py
+++ data_analize/test_poom_map.py
@@ -2,21 +2,24 @@
 import pandas as pd
 import geopandas as gpd
 import shapely
+import plotly.graph_objects as go
 # needs 'descartes'
 
 import matplotlib.pyplot as plt
-df = pd.read_csv("data_analize/data/laclon.csv")
-gdf = gpd.GeoDataFrame(df.drop(['lac', 'lon'], axis=1),
-                       crs={'init': 'epsg:4326'},
-                       geometry=[shapely.geometry.Point(xy)
-                                 for xy in zip(df.lon, df.lac)])
-print(gdf)
+df = pd.read_csv("dashborad_with_my_team\data_analize\data\laclon.csv")
+fig = go.Figure(data=go.Scattergeo(
+        lon = df['lon'],
+        lat = df['lac'],
+        text = df['ชื่อ'],
+        mode = 'markers',
+        marker_color = df['ColorMap'],
+        ))
 
-world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
-base = world.plot(color='white', edgecolor='black')
-gdf.plot(ax=base, marker='o', color='red', markersize=5)
-
-plt.show()
+fig.update_layout(
+        title = 'Most trafficked US airports<br>(Hover for airport names)',
+        geo_scope='th',
+    )
+fig.show()
 # df = pd.DataFrame({'city': ['Berlin', 'Paris', 'Munich'],
 #                    'latitude': [52.518611111111, 48.856666666667, 48.137222222222],
 #                    'longitude': [13.408333333333, 2.3516666666667, 11.575555555556]})
